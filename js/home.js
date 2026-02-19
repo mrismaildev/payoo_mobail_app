327197c50505e857bccece5173c361608674ef43
mrismaildev <ismail.dev.khulna@gmail.com>
2026-02-19 19:46:06 +0600

    added new update

--- js/home.js
+++ js/home.js
@@ -1,32 +1,37 @@
-console.log('I am connected')
+console.log('I am connected');
 
+const btnAddMoney = document.getElementById('btnAddMoney');
+const availableBalance = document.getElementById('availableBalance').innerText;
+const balanceNumber = parseFloat(availableBalance);
+const sendtoast = document.querySelector('.sendtoast');
 
-const btnAddMoney = document.getElementById('btnAddMoney')
-const availableBalance = document.getElementById('availableBalance').innerText
-const balanceNumber = parseFloat(availableBalance)
-const sendtoast = document.querySelector('.sendtoast')
-
-btnAddMoney.addEventListener('click', function () {
-  console.log("I am click ")
-  const pinNumber = getInputValue('pinNumber')
-  const addAmount = getInputValue('addAmount')
-  const accountNumber = getInputValue('accountNumber')
-  const selectBank = document.getElementById('selectBank').value
-  console.log(balanceNumber, pinNumber, addAmount, accountNumber, selectBank)
+btnAddMoney.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('I am click ');
+  const pinNumber = getInputValue('pinNumber');
+  const addAmount = getInputValue('addAmount');
+  const accountNumber = getInputValue('accountNumber');
+  const selectBank = document.getElementById('selectBank').value;
+  // console.log(balanceNumber, pinNumber, addAmount, accountNumber, selectBank);
   if (!pinNumber || !addAmount || !accountNumber || !selectBank) {
-    document.getElementById('hiddenToast').classList.remove('hidden')
-    sendtoast.innerText = 'Please fil the all field'
-    return
+    document.getElementById('hiddenToast').classList.remove('hidden');
+    sendtoast.innerText = 'Please fil the all field';
+    return;
+  } else {
+    document.getElementById('hiddenToast').classList.add('hidden');
   }
-  else {
-    document.getElementById('hiddenToast').classList.add('hidden')
-  }
-if (accountNumber.length !== 11 || isNaN(accountNumber)) {
+  if (isNaN(accountNumber)) {
     alert('incorrect Bank Account Number');
-}
-else {
-
-    alert('success')
+    return;
   }
-
-})
+  if (pinNumber === 1234) {
+    const newBalalnce = balanceNumber + addAmount;
+    document.getElementById('availableBalance').innerText = newBalalnce;
+    sendtoast.classList.add('hidden');
+    const successTOAST = document.getElementById('succsess');
+    successTOAST.classList.remove('hidden')
+    successTOAST.innerText = 'Add Money Success'
+    console.log(successTOAST);
+  
+  }
+});
